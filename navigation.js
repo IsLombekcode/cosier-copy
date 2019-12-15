0043ea43f826b7a64fe17406c2caa96b77f53dfe
islombekturgunov0799@gmail.com <57730843+IsLombekcode@users.noreply.github.com>
2019-12-15 20:10:09 +0500

    Cosier

---
+++ navigation.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const panel = document.querySelector(".panel__box");
+    const line = document.querySelector("div .navigation__button-inner");
+    const close = document.querySelector("div .panel__button-text");
+    line.addEventListener("click", () => { open() });
+    close.addEventListener("click", () => { closed() });
+    const open = () => {
+        panel.style.transform = "translateX(0px)";
+    }
+    const closed = () => {
+        panel.style.transform = "translateX(320px)";
+    }
+})
